20d179226081e8fd98b88601d8ce6bffa5809d4a
arnaris <32923729+arnaris@users.noreply.github.com>
2017-10-20 01:03:00 +0700

    V2.0001

--- bot.php
+++ bot.php
@@ -2,12 +2,6 @@
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('<reply token>', $textMessageBuilder);
-if ($response->isSucceeded()) {
-    echo 'Succeeded!';
-    return;
-}
+$response = $bot->replyText('<reply token>', 'hello!');
 
 ?>
