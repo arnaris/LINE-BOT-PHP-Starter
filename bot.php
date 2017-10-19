bf56c38552b18bb36f2690a589dc0df80c24c96b
arnaris <32923729+arnaris@users.noreply.github.com>
2017-10-20 00:55:30 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,8 +1,13 @@
 <?php
 
-echo "I am a bot";
-echo "<BR>";
-echo $_ENV["LINEBOT_ACCESS_TOKEN"];
-echo "<BR>";
-echo $_ENV["LINEBOT_CHANNEL_SECRET"];
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage('<reply token>', $textMessageBuilder);
+if ($response->isSucceeded()) {
+    echo 'Succeeded!';
+    return;
+}
+
 ?>
