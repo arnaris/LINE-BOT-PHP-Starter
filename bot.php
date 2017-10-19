fc403e585a3a67f4e76d0363ee43458a285b26d3
arnaris <32923729+arnaris@users.noreply.github.com>
2017-10-20 01:04:46 +0700

    Uploaded via desktop

--- bot.php
+++ bot.php
@@ -1,7 +1,10 @@
 <?php
 
+/*
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
 $response = $bot->replyText('<reply token>', 'hello!');
+*/
 
+ecoh "TEST TEST";
 ?>
