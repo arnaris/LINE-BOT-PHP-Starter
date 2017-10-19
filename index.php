203c70b219935972a6fd7d5633bb0b2e68001b4e
arnaris <32923729+arnaris@users.noreply.github.com>
2017-10-20 01:14:56 +0700

    ...

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 echo "Hello LINE BOT!!!";
+echo "<BR>";
+echo $_ENV["LINEBOT_ACCESS_TOKEN"];
+echo "<BR>";
+echo $_ENV["LINEBOT_CHANNEL_SECRET"];
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
