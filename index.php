83e59d55b6c408e77c96d3f8a3b084b4ed8584a7
arnaris <32923729+arnaris@users.noreply.github.com>
2017-10-21 01:16:53 +0700

    .

--- index.php
+++ index.php
@@ -70,22 +70,24 @@ foreach ($client->parseEvents() as $event) {
                             array(
                               'type' => 'text',
                               'text' => $message['text']
-                            )                            
+                            )
                           )
                     ));
 
+                    /*
                     $client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
                         'messages' => array(
                             array(
                               'type': 'location',
                               'title': 'my location',
-                              'address': '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
+                              'address': 'Test Test Test',
                               'latitude': 35.65910807942215,
                               'longitude': 139.70372892916203
                             )
                         )
                     ));
+                    */
                     break;
                 default:
                     error_log("Unsupporeted message type: " . $message['type']);
