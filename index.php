3160d0f24040154b7eb11eae545b45447438d114
arnaris <32923729+arnaris@users.noreply.github.com>
2017-10-21 00:57:36 +0700

    .

--- index.php
+++ index.php
@@ -64,10 +64,19 @@ foreach ($client->parseEvents() as $event) {
                         'replyToken' => $event['replyToken'],
                         'messages' => array(
                             array(
+
+                              /*
                               'type' => 'text',
                               'text' => $message['text']
+                              */
 
+                              "type": "location",
+                              "title": "my location",
+                              "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+                              "latitude": 35.65910807942215,
+                              "longitude": 139.70372892916203
 
+                              
                                 /*
                                 "type" => "template",
                                 "altText" => "this is a image carousel template",
