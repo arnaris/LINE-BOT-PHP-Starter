d516712d400eecc55de4e1d59a5fb1644c5bec8c
arnaris <32923729+arnaris@users.noreply.github.com>
2017-10-20 23:05:46 +0700

    .

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
 
-/*
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
 
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
+try {
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  eco "error";
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  eco "error";
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  eco "error";
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  eco "error";
+}
 
 foreach ($events as $event) {
 
@@ -13,7 +24,7 @@ foreach ($events as $event) {
   $bot->replyMessage($event->getReplyToken(), $outputText);
 
 }
-*/
+
 echo $_ENV["LINEBOT_ACCESS_TOKEN"];
 echo $_ENV["LINEBOT_CHANNEL_SECRET"];
 
