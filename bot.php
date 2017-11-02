c6f7a714f83e14e9315e5c5cf4554b644fedbc4d
arnaris <32923729+arnaris@users.noreply.github.com>
2017-11-03 01:56:48 +0700

    .

--- bot.php
+++ bot.php
@@ -1,3 +1,23 @@
 <?php
-ecoh "TEST TEST";
+
+require_once('./LINEBotTiny.php');
+$channelAccessToken = $_ENV["LINEBOT_ACCESS_TOKEN"];
+$channelSecret = $_ENV["LINEBOT_CHANNEL_SECRET"];
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+$userID = "R9bf96e170c4d75c57390c1b21dc458c0";
+
+$res = $bot->getProfile($userID);
+if ($res->isSucceeded()) {
+    $profile = $res->getJSONDecodedBody();
+    $displayName = $profile['displayName'];
+    $statusMessage = $profile['statusMessage'];
+    $pictureUrl = $profile['pictureUrl'];
+
+    echo $displayName . "<BR>";
+    echo $statusMessage . "<BR>";
+    echo "<img src=" . $pictureUrl .">";
+}
+
+
 ?>
