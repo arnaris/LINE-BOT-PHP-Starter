6d536380f43ec0896f8794b8fb65085798390c64
arnaris <32923729+arnaris@users.noreply.github.com>
2018-02-02 14:56:30 +0700

    Add files via upload

--- broadcast.php
+++ broadcast.php
@@ -62,10 +62,20 @@ if ($_POST["formSubmit"] == "yes") {
 			}
 		}
 
-		echo '<pre>'; print_r($columns); echo '</pre>';
+		if (count($columns)>0) {
 
+			array_push($columns, array(
+				                        'imageUrl' => 'https://s3-ap-southeast-1.amazonaws.com/thinktank-assets/NewsPic/0000.png',
+				                        'action' => array(
+				                              'type' => 'uri',
+				                              'label' => 'Visit',
+				                              'uri' => 'https://scbcorp.sharepoint.com/sites/scbthinktank/'
+				                        )
+				                    ));
 
-		/*
+		}
+
+		//echo '<pre>'; print_r($columns); echo '</pre>';
 
 		$client->pushMessage(array(
 				'to' => $to,
@@ -75,98 +85,16 @@ if ($_POST["formSubmit"] == "yes") {
 		            'altText' => 'Weekly market intelligence',
 		            'template' => array(
 		                'type' => 'image_carousel',
-		                'columns' => array(
-		                    array(
-		                        'imageUrl' => 'https://s3-ap-southeast-1.amazonaws.com/thinktank-assets/20180126/0927.png',
-		                        'action' => array(
-		                              'type' => 'uri',
-		                              'label' => 'Full article',
-		                              'uri' => 'https://scbcorp.sharepoint.com/sites/scbthinktank/SitePages/article.aspx?NewListID=927'
-		                        )
-		                    ),
-												array(
-		                        'imageUrl' => 'https://s3-ap-southeast-1.amazonaws.com/thinktank-assets/20180126/0933.png',
-		                        'action' => array(
-		                              'type' => 'uri',
-		                              'label' => 'Full article',
-		                              'uri' => 'https://scbcorp.sharepoint.com/sites/scbthinktank/SitePages/article.aspx?NewListID=933'
-		                        )
-												),
-		                    array(
-		                        'imageUrl' => 'https://s3-ap-southeast-1.amazonaws.com/thinktank-assets/20180126/0935.png',
-		                        'action' => array(
-		                              'type' => 'uri',
-		                              'label' => 'Full article',
-		                              'uri' => 'https://scbcorp.sharepoint.com/sites/scbthinktank/SitePages/article.aspx?NewListID=935'
-		                        )
-		                    ),
-		                    array(
-		                        'imageUrl' => 'https://s3-ap-southeast-1.amazonaws.com/thinktank-assets/20180126/0923.png',
-		                        'action' => array(
-		                              'type' => 'uri',
-		                              'label' => 'Full article',
-		                              'uri' => 'https://scbcorp.sharepoint.com/sites/scbthinktank/SitePages/article.aspx?NewListID=923'
-		                        )
-		                    ),
-		                    array(
-		                        'imageUrl' => 'https://s3-ap-southeast-1.amazonaws.com/thinktank-assets/20180126/0887.png',
-		                        'action' => array(
-		                              'type' => 'uri',
-		                              'label' => 'Full article',
-		                              'uri' => 'https://scbcorp.sharepoint.com/sites/scbthinktank/SitePages/article.aspx?NewListID=887'
-		                        )
-												),
-		                    array(
-		                        'imageUrl' => 'https://s3-ap-southeast-1.amazonaws.com/thinktank-assets/20180126/0913.png',
-		                        'action' => array(
-		                              'type' => 'uri',
-		                              'label' => 'Full article',
-		                              'uri' => 'https://scbcorp.sharepoint.com/sites/scbthinktank/SitePages/article.aspx?NewListID=913'
-		                        )
-												),
-		                    array(
-		                        'imageUrl' => 'https://s3-ap-southeast-1.amazonaws.com/thinktank-assets/20180126/0920.png',
-		                        'action' => array(
-		                              'type' => 'uri',
-		                              'label' => 'Full article',
-		                              'uri' => 'https://scbcorp.sharepoint.com/sites/scbthinktank/SitePages/article.aspx?NewListID=920'
-		                        )
-													),
-													array(
-			                        'imageUrl' => 'https://s3-ap-southeast-1.amazonaws.com/thinktank-assets/20180126/0905.png',
-			                        'action' => array(
-			                              'type' => 'uri',
-			                              'label' => 'Full article',
-			                              'uri' => 'https://scbcorp.sharepoint.com/sites/scbthinktank/SitePages/article.aspx?NewListID=905'
-			                        )
-														),
-												array(
-			                        'imageUrl' => 'https://s3-ap-southeast-1.amazonaws.com/thinktank-assets/20180126/0891.png',
-			                        'action' => array(
-			                              'type' => 'uri',
-			                              'label' => 'Full article',
-			                              'uri' => 'https://scbcorp.sharepoint.com/sites/scbthinktank/SitePages/article.aspx?NewListID=891'
-			                        )
-														),
-		                    array(
-		                        'imageUrl' => 'https://s3-ap-southeast-1.amazonaws.com/thinktank-assets/20171103/010.png',
-		                        'action' => array(
-		                              'type' => 'uri',
-		                              'label' => 'Visit',
-		                              'uri' => 'https://scbcorp.sharepoint.com/sites/scbthinktank/'
-		                        )
-		                    )
-		                )
+		                'columns' => $columns
 		            )
 		        )
 		    )
 		));
 
-		*/
 
 	} else {
 
-		echo "Status: wrong password";
+		echo "Status: Wrong password";
 
 	}
 }
