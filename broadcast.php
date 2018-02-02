d22feec92622c7502a5d6a8151c16a1b9c421df3
arnaris <32923729+arnaris@users.noreply.github.com>
2018-02-02 14:32:35 +0700

    Add files via upload

--- broadcast.php
+++ broadcast.php
@@ -1,3 +1,27 @@
+<div style="margin: 20px; border: dashed 2px #ccc; width: 300px;">
+	<div style="margin: 15px;"><B>INSTRUCTION:</B> Fill in the news ID (max 8). Choose type of message. Fill the password. Click send.</div>
+	<form action="" method="post">
+    <div style="margin: 15px;">News ID:
+        <div style="margin: 3px;">1) <input type="text" name="1" value="<? echo $_POST["1"]; ?>"></div>
+        <div style="margin: 3px;">2) <input type="text" name="2" value="<? echo $_POST["2"]; ?>"></div>
+        <div style="margin: 3px;">3) <input type="text" name="3" value="<? echo $_POST["3"]; ?>"></div>
+        <div style="margin: 3px;">4) <input type="text" name="4" value="<? echo $_POST["4"]; ?>"></div>
+        <div style="margin: 3px;">5) <input type="text" name="5" value="<? echo $_POST["5"]; ?>"></div>
+        <div style="margin: 3px;">6) <input type="text" name="6" value="<? echo $_POST["6"]; ?>"></div>
+        <div style="margin: 3px;">7) <input type="text" name="7" value="<? echo $_POST["7"]; ?>"></div>
+        <div style="margin: 3px;">8) <input type="text" name="8" value="<? echo $_POST["8"]; ?>"></div>
+    </div>
+		<div style="margin: 15px;">Type of message:
+			<input type="radio" name="type" value="test" checked="checked">Test |
+	  	<input type="radio" name="type" value="real">Real
+		</div>
+		<div style="margin: 15px;">Password: <input type="text" name="password"></div>
+		<div style="margin: 15px;"><input type="submit" value="Submit"></div>
+		<input type="hidden" name="formSubmit" value="yes">
+	</form>
+</div>
+
+
 <?php
 
 require_once('./LINEBotTiny.php');
@@ -14,13 +38,24 @@ if ($_POST["formSubmit"] == "yes") {
 
 			//$to = "C1bf97c635541efd0d001ecbffd684625";
 			$to = "R9bf96e170c4d75c57390c1b21dc458c0";
-			echo "Real message push sent";
+			echo "Status: Real message sent";
 
 		} else {
 
 			$to = "R9bf96e170c4d75c57390c1b21dc458c0";
-			echo "Test message push sent";
+			echo "Status: Test message sent";
 		}
+
+
+		for($i=1;$i<9;$i++) {
+			if ($_POST[$i] != "") {
+				echo trim($_POST[$i]);
+			}
+		}
+
+
+
+		/*
 
 		$client->pushMessage(array(
 				'to' => $to,
@@ -117,15 +152,13 @@ if ($_POST["formSubmit"] == "yes") {
 		    )
 		));
 
+		*/
+
+	} else {
+
+		echo "Status: wrong password";
+
 	}
 }
 
 ?>
-
-<form action="" method="post">
-	<input type="radio" name="type" value="test">Test<br>
-  <input type="radio" name="type" value="real">Real<br>
-  Password: <input type="text" name="password">
- 	<input type="hidden" name="formSubmit" value="yes">
- 	<input type="submit" value="Submit">
-</form>
